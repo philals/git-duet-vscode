26fcde0aa896ac2156ca0a93701629a0db515599
Phil Alsford <phil.alsford@xero.com>
2018-06-16 21:35:24 +1200

    comments
    Co-authored-by: Amy Martin <amy.martin@xero.com>
    Co-authored-by: Bryan Tee <bryan.tee@xero.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,4 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { printNames } from './duetFunctions';
 import { spawn } from './spawnCommandAsync';
@@ -8,10 +6,6 @@ import { spawn } from './spawnCommandAsync';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "git-duet" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
