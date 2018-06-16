8c624b4b33a71e7e5c91020150d1bcfc2d11a573
Phil Alsford <phil.alsford@xero.com>
2018-06-16 20:34:10 +1200

    Adds comman to git solo

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as spawnCommand from 'spawn-command';
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
@@ -14,11 +15,24 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.gitSolo', async () => {
         // The code you place here will be executed every time your command is executed
+        const value = await vscode.window.showInputBox({ prompt: 'Who is solo?' });
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        let child = spawnCommand('git solo ' + value);
+
+        child.stdout.on('data', function (data: string) {
+
+            let allLines = data.toString();
+            let firstLine = allLines.split('\n')[0];
+            let name = firstLine.split('=')[1].replace(`'`, '').replace(`'`, '');
+
+            vscode.window.showInformationMessage("git solo: " + name);
+        });
+
+        child.on('exit', function (exitCode: string) {
+            console.log('exit', exitCode);
+        });
     });
 
     context.subscriptions.push(disposable);
