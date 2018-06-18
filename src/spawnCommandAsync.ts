db11a2fbdacd1bafc4481efb235ea52236eb7db5
Phil Alsford <phil.alsford@xero.com>
2018-06-18 14:21:17 +1200

    Changes to shelljs for Windows

--- src/spawnCommandAsync.ts
+++ src/spawnCommandAsync.ts
@@ -1,17 +1,18 @@
-import * as spawnCommand from 'spawn-command';
+import * as spawnCommand from 'shelljs';
 
 export async function spawn(command: string): Promise<string> {
    return new Promise<string>(function (resolve, reject) {
 
-      let child = spawnCommand(command);
+      let child = spawnCommand.exec(command);
+      return resolve(child.stdout);
 
-      child.stdout.on('data', function (data: string) {
-         return resolve(data.toString());
-      });
+      // child.stdout.on('data', function (data: string) {
+      //    return resolve(data.toString());
+      // });
 
-      child.on('exit', function (exitCode: string) {
-         return reject(exitCode);
-      });
+      // child.on('exit', function (exitCode: string) {
+      //    return reject(exitCode);
+      // });
    });
 }
 
